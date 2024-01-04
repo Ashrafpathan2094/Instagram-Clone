aa7b34a6b87746290795e078a98ebf8dc63cce24
ashraf khan <minazpathan2094@gmail.com>
2024-01-04 11:55:47 +0530

    profile page header and tabs added

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/Homepage";
 import Authpage from "./pages/AuthPage/Authpage";
 import PageLayout from "./Layouts/PageLayout/PageLayout";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 function App() {
   return (
     <PageLayout>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/auth" element={<Authpage />} />
+        <Route path="/:username" element={<ProfilePage />} />
       </Routes>
     </PageLayout>
   );
---
+++ src/components/Profile/ProfileHeader.tsx
@@ -0,0 +1,76 @@
+import {
+  Avatar,
+  AvatarGroup,
+  Button,
+  Flex,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+
+const ProfileHeader = () => {
+  return (
+    <Flex
+      gap={{ base: 4, sm: 10 }}
+      py={10}
+      direction={{ base: "column", sm: "row" }}
+    >
+      <AvatarGroup
+        size={{ base: "xl", md: "2xl" }}
+        justifySelf={"center"}
+        alignSelf={"flex-start"}
+        mx={"auto"}
+      >
+        <Avatar name="Array_Ashraf" src="/profilepic.png" />
+      </AvatarGroup>
+      <VStack alignItems={"start"} gap={2} mx={"auto"} flex={1}>
+        <Flex
+          gap={4}
+          direction={{ base: "column", sm: "row" }}
+          justifyContent={{ base: "center", sm: "flex-start" }}
+          alignItems={"center"}
+          w={"full"}
+        >
+          <Text fontSize={{ base: "sm", md: "lg" }}>Array_Ashraf</Text>
+          <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
+            <Button
+              bg={"white"}
+              color={"black"}
+              _hover={{ bg: "whiteAlpha.800" }}
+              size={{ base: "xs", md: "md" }}
+            >
+              Edit Profile
+            </Button>
+          </Flex>
+        </Flex>
+        <Flex alignItems={"center"} gap={{ base: 2, sm: 4 }}>
+          <Text fontSize={{ base: "xs", md: "sm" }}>
+            <Text as="span" fontWeight={"bold"} mr={1}>
+              4
+            </Text>
+            Posts
+          </Text>
+          <Text fontSize={{ base: "xs", md: "sm" }}>
+            <Text as="span" fontWeight={"bold"} mr={1}>
+              10
+            </Text>
+            Following
+          </Text>
+          <Text fontSize={{ base: "xs", md: "sm" }}>
+            <Text as="span" fontWeight={"bold"} mr={1}>
+              1.4M
+            </Text>
+            Followers
+          </Text>
+        </Flex>
+        <Flex alignItems={"center"} gap={4}>
+          <Text fontSize={"sm"} fontWeight={"bold"}>
+            Array_Ashraf
+          </Text>
+        </Flex>
+        <Text fontSize={"sm"}>Just Your Average Developer</Text>
+      </VStack>
+    </Flex>
+  );
+};
+
+export default ProfileHeader;
