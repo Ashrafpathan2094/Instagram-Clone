36ad0268b6465a2921bd04abfb32e5bca0cc3501
Ashraf Khan <2094ashrafkhan@gmail.com>
2024-03-11 16:19:57 +0530

    create post func added

---
+++ src/store/postStore.ts
@@ -0,0 +1,7 @@
+import { create } from "zustand";
+
+const usePostStore = create((set) => ({
+  posts: [],
+  createPost: (post) => set((state) => ({ posts: [post, ...state.posts] })),
+}));
+export default usePostStore;
--- src/store/userProfileStore.ts
+++ src/store/userProfileStore.ts
@@ -3,7 +3,14 @@ import { create } from "zustand";
 const useUserProfileStore = create((set) => ({
   userProfile: null,
   setUserProfile: (userProfile) => set({ userProfile }),
-  // addPost:()
+  // this is used to update the number of posts in the profile page
+  addPost: (post) =>
+    set((state) => ({
+      userProfile: {
+        ...state.userProfile,
+        post: [post.id, ...state.userProfile.posts],
+      },
+    })),
 }));
 
 export default useUserProfileStore;
